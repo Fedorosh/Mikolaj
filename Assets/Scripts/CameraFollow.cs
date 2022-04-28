50f71afdf2259251b3ddc6d8b12f6bb60f98d6bf
Janosz Zieliński <janosz@epicvr.pl>
2022-04-28 23:09:54 +0200

    refactor following camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    [SerializeField] private Transform playerTransform;
     [SerializeField] private Transform transformToFollow;
-    [SerializeField] private float positionSpeed = 10f;
-    [SerializeField] private float rotationSpeed = 100f;
+    [SerializeField] private float smoothTime = 10f;
 
-    private Vector3 startingCameraPosition;
+    private Vector3 startingCameraPosition = Vector3.zero;
 
     private void Start()
     {
-        startingCameraPosition = transform.position;
+        //startingCameraPosition = transform.position - transformToFollow.position;
     }
 
     private void LateUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, transformToFollow.position, positionSpeed * Time.deltaTime);
-        transform.rotation = Quaternion.Lerp(transform.rotation, transformToFollow.rotation, rotationSpeed * Time.deltaTime);
+        transform.position = Vector3.SmoothDamp(transform.position,transformToFollow.position,ref startingCameraPosition,smoothTime * Time.deltaTime);
+        //transform.rotation = Quaternion.Lerp(transform.rotation,playerTransform.rotation,rotationSpeed * Time.deltaTime);
+        transform.LookAt(playerTransform);
     }
 }
