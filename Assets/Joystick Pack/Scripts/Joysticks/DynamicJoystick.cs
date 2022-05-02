7f0445c986676958263b384e926b3e2eb6c3db8a
Janosz Zieliński <janosz@epicvr.pl>
2022-05-02 17:00:09 +0200

    fix joystick, make debug console on android

--- Assets/Joystick Pack/Scripts/Joysticks/DynamicJoystick.cs
+++ Assets/Joystick Pack/Scripts/Joysticks/DynamicJoystick.cs
@@ -18,6 +18,7 @@ public class DynamicJoystick : Joystick
 
     public override void OnPointerDown(PointerEventData eventData)
     {
+        Debug.LogError("PointedDown");
         background.anchoredPosition = ScreenPointToAnchoredPosition(eventData.position);
         background.gameObject.SetActive(true);
         base.OnPointerDown(eventData);
